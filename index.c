07103d996a1a372a868c7c03529c0f127d62d7d3
Aditya Pudipeddi <adityapudipeddi@gmail.com>
2018-12-17 03:13:02 +0530

    Creation successful

--- index.c
+++ index.c
@@ -15,6 +15,7 @@ int main()
   const char delimiters[10] = "' ',','";
   char *p1, *p2;
   char *createParsed[10], *databaseParsed[5];
+  char trash1[30], trash2[30];
 
   printf("Enter your query to view, create or select databases: \n");
   fgets(query, 40, stdin);
@@ -26,6 +27,10 @@ int main()
     p1 = strtok(NULL, delimiters);
   }
   printf("%s", databaseParsed[1]); //database-name
+  for (i = 0; i < (strlen(databaseParsed[1]) - 1); i++)
+  {
+    databaseName[i] = databaseParsed[1][i];
+  }
 
   // Database creation or selection
   switch (query[0])
@@ -78,12 +83,24 @@ int main()
         p2 = strtok(NULL, delimiters);
       }
       printf("%s\n", createParsed[2]); //table-name
+      strcat(path, databaseName);
+      for (i = 0; i < strlen(path); i++)
+      {
+        trash1[i] = path[i];
+      }
+      strcat(trash1, "/tables.txt");
+      printf("%s", trash1);
+      fp = fopen(trash1, "a");
+      fprintf(fp, "%s", createParsed[2]);
 
-      strcat(path, databaseParsed[1]);
-      strcat(path, "tables.txt");
-      printf("%s", path);
-      fp = fopen(path, "a");
-
+      for (i = 0; i < strlen(path); i++)
+      {
+        trash2[i] = path[i];
+      }
+      strcat(trash2, "/columns.txt");
+      printf("%s", trash2);
+      fp = fopen(trash2, "a");
+      fprintf(fp, "%s %s", createParsed[2], createParsed[3]);
 
       break;
     }
